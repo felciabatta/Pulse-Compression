328cb746a463c4b6b27da6a07ab110cf1cad7c11
FelixxDP <felix.dp.2020@bristol.ac.uk>
2022-10-24 19:59:09 +0100

    added filter_example method to massively streamline running filter combinations, and tidy main code; auto color map scaling; improved wien function

--- compress.py
+++ compress.py
@@ -21,6 +21,8 @@ from tkinter import filedialog
 root = tk.Tk()
 root.withdraw()
 
+filter_method = {"match": 1, "wien": 2, 'match-wien': 12, 'wien-match': 21}
+
 
 class signal:
     """
@@ -110,7 +112,7 @@ class signal:
 
         self.match2d()
 
-    def match(self, remove_pulse=False, s1=None, s2=None, delay=2, trim=0):
+    def match1d(self, remove_pulse=False, s1=None, s2=None, delay=2, trim=0):
         if s1 is None:
             s1 = np.copy(self.data_b)
         if s2 is None:
@@ -125,8 +127,9 @@ class signal:
                                      mode='same')/(0.1*len(s2))
         self.results = out
 
-    def match2d(self, remove_pulse=False, s1=None, s2=None, delay=2, trim=0):
-        # identical to match()
+    def match2d(self, s1=None, s2=None, remove_pulse=False,
+                delay=2, trim=0, remove_matchedpulse=0):
+        # identical to match1d()
         if s1 is None:
             s1 = np.copy(self.data_b)
         if s2 is None:
@@ -135,20 +138,104 @@ class signal:
         if remove_pulse:
             self.remove_excitation(s1, s2, delay=delay, trim=trim)
 
-        out = sg.correlate(s1, s2.reshape((-1, 1)),
-                           mode='same')/(0.1*len(self.data_a))
-        self.results = out
+        self.results = sg.correlate(s1, s2.reshape((-1, 1)),
+                                    mode='same')/(0.1*len(self.data_a))
 
-    def wien(self, signal=None, window=None, power=None):
-        if signal is None:
-            signal = self.results
-        self.results = sg.wiener(signal, window, power)
+        if remove_matchedpulse:
+            self.remove_datapoints(ub=remove_matchedpulse)
+
+    def wien(self, s1=None, s2=None, remove_pulse=False,
+             delay=2, trim=0, window=None, power=None):
+        if s1 is None:
+            s1 = np.copy(self.data_b)
+        if s2 is None:
+            s2 = np.copy(self.data_a)
+
+        if remove_pulse:
+            self.remove_excitation(s1, s2, delay=delay, trim=trim)
+
+        self.results = sg.wiener(s1, window, power)
 
     def remove_excitation(self, s1, s2, delay=2, trim=0):
+        print('trim is'+str(trim))
         if delay == None:
             delay = np.argmax(s1[:, 1]) - np.argmax(s2)
         s1[delay:delay+len(s2)+trim, :] = 0
-        pass
+
+    def remove_datapoints(self, signal=None, lb=None, ub=None, newval=0):
+        if signal is None:
+            signal = self.results
+        signal[lb:ub, :] = newval
+
+    def get_max(self, s1=None, s2=None, remove_pulse=1, delay=2, trim=0):
+        if s1 is None:
+            s1 = np.copy(self.data_b)
+        else:
+            s1 = np.copy(s1)
+
+        if s2 is None:
+            s2 = np.copy(self.data_a)
+        else:
+            s1 = np.copy(s2)
+
+        if remove_pulse:
+            self.remove_excitation(s1, s2, delay=delay, trim=trim)
+
+        return np.max(s1)
+
+    def filter_example(self, filter_method=1, signal=None,
+                       remove_pulse=1, trim=0, remove_matchedpulse=0,
+                       window=(100, 10),
+                       title='Title', x=10, MIN=0, MAX=None,
+                       plotresults=(1, 1, 1, 1, 1)):
+
+        if signal is None:
+            signal = self.data_b
+
+        if filter_method == 0:
+            None
+
+        elif filter_method == 1:
+            self.match2d(signal, remove_pulse=remove_pulse, trim=trim,
+                         remove_matchedpulse=remove_matchedpulse)
+
+        elif filter_method == 2:
+            self.wien(signal, remove_pulse=remove_pulse, trim=trim, window=(100, 10))
+
+        elif filter_method == 12:
+            self.match2d(signal, remove_pulse=remove_pulse, trim=trim,
+                         remove_matchedpulse=remove_matchedpulse)
+            self.wien(self.results, window=(100, 10))
+
+        elif filter_method == 21:
+            self.wien(signal, remove_pulse=remove_pulse, trim=trim,
+                      window=(100, 10))
+            self.match2d(self.results,
+                         remove_matchedpulse=remove_matchedpulse)
+
+        if MAX is None:
+            MAXbf = 0.7*self.get_max()
+            MAXaf = 0.7*self.get_max(self.results)
+        else:
+            MAXbf = MAX
+            MAXaf = MAX
+
+        plotfunc = [self.plot1d, self.plot1d, self.plot1d,
+                    self.plot2d, self.plot2d]
+
+        plotargs = [{'title': title},
+                    {'data': self.data_b[:, x], 't':self.t_b, 'title':title},
+                    {'data': self.results[:, x], 't':self.t_b, 'title':title},
+                    {'MIN': MIN, 'MAX': MAXbf, 'title': title},
+                    {'data': self.results, 'MIN': MIN, 'MAX': MAXaf, 'title': title}]
+
+        plots = [None]*5
+
+        for i, _ in enumerate(plotresults):
+            if plotresults[i]:
+                plots[i] = plotfunc[i](**plotargs[i])
+
+        return plots
 
     def plot1d(self, data=None, t=None, i0=None, iend=None, title=r'Signal',
                xlabel=r'Time, $t\,$seconds', ylabel=r'Amplitude, $A$'):
@@ -157,7 +244,7 @@ class signal:
             t = self.t_a[i0:iend]
 
         plot = LaPlot(plt.plot, [t, data],
-                      {'color':mycols['sweetpink'], 'linewidth':1},
+                      {'color': mycols['sweetpink'], 'linewidth': 1},
                       xlim=(t[0], t[-1]), title=title,
                       xlabel=xlabel, ylabel=ylabel, showgrid=1)
         return plot
@@ -171,7 +258,7 @@ class signal:
             x = self.x
         plot = LaPlot(plt.pcolormesh, [data], {"vmin": MIN, "vmax": MAX},
                       title=title, xlabel=xlabel, ylabel=ylabel, showgrid=0,
-                      figsize=(4,6))
+                      figsize=(4, 6))
         return plot
 
     def save_results(self, data=None, filename=None):
--- matchfilter_examples.py
+++ matchfilter_examples.py
@@ -5,8 +5,9 @@ Created on Tue Oct 11 23:06:59 2022
 
 @author: felixdubicki-piper
 """
+from compress import signal
+import numpy as np
 
-from compress import *
 x = 10  # slice to plot in 1D
 
 # %% CLEAN U.S. PULSE, 2MHz
@@ -15,16 +16,7 @@ pulse = signal("signal_data/pulse_2MHznonoise/pulse2mhz.dat",
                "signal_data/pulse_2MHznonoise/bscan.dat")
 title = r'Clean U.S. Pulse, $2\,$MHz'
 
-pulse.match2d(remove_pulse=1)
-
-plot = pulse.plot1d(title=title)
-
-plotbf = pulse.plot1d(pulse.data_b[:, x], pulse.t_b, title=title)
-plotaf = pulse.plot1d(pulse.results[:, x], pulse.t_b, title=title)
-
-mapbf = pulse.plot2d(MIN=0, MAX=0.04, title=title)
-mapaf = pulse.plot2d(pulse.results, MIN=0, MAX=0.04, title=title)
-
+plots = pulse.filter_example(filter_method=21, title=title, x=x, MAX=0.06)
 
 # %% U.S. PULSE, 1MHz
 
@@ -32,17 +24,7 @@ pulse1 = signal("signal_data/pulse_1MHznoise/pulse1mhz.dat",
                 "signal_data/pulse_1MHznoise/bscanpulse1.dat")
 title = r'U.S. Pulse, $1\,$MHz'
 
-pulse1.match2d(remove_pulse=1)
-pulse1.wien(window=(100, 10))
-
-plot = pulse1.plot1d(title=title)
-
-plotbf = pulse1.plot1d(pulse1.data_b[:, x], pulse1.t_b, title=title)
-plotaf = pulse1.plot1d(pulse1.results[:, x], pulse1.t_b, title=title)
-
-mapbf = pulse1.plot2d(MIN=0, MAX=0.08, title=title)
-mapaf = pulse1.plot2d(pulse1.results, MIN=0, MAX=0.08, title=title)
-
+plots = pulse1.filter_example(filter_method=21, title=title, x=x)
 
 # %% U.S. PULSE, 2MHz
 
@@ -50,17 +32,7 @@ pulse2 = signal("signal_data/pulse_2MHznoise/pulse2mhz.dat",
                 "signal_data/pulse_2MHznoise/bscanpulse2.dat")
 title = r'U.S. Pulse, $2\,$MHz'
 
-pulse2.match2d(remove_pulse=1, trim=80)
-# pulse2.wien(window=(100, 10))
-
-plot = pulse2.plot1d(title=title)
-
-plotbf = pulse2.plot1d(pulse2.data_b[:, x], pulse2.t_b, title=title)
-plotaf = pulse2.plot1d(pulse2.results[:, x], pulse2.t_b, title=title)
-
-mapbf = pulse2.plot2d(MIN=0, MAX=0.02, title=title)
-mapaf = pulse2.plot2d(pulse2.results, MIN=0, MAX=0.02, title=title)
-
+plots = pulse2.filter_example(filter_method=21, title=title, trim=80, x=x)
 
 # %% Short Chirp, 0.8-2.2MHz
 
@@ -68,17 +40,7 @@ chirpS = signal("signal_data/chirp_0822MHz_2u/chirpsignal.dat",
                 "signal_data/chirp_0822MHz_2u/bscanchirp0822.dat")
 title = r'Chirp Pulse, $0.8\,$-$\,2.2\,$MHz, $2\,\mu$s'
 
-chirpS.match2d(remove_pulse=1, trim=100)
-# chirpS.wien(window=(100, 10))
-
-plot = chirpS.plot1d(title=title)
-x=0
-plotbf = chirpS.plot1d(chirpS.data_b[:, x], chirpS.t_b, title=title)
-plotaf = chirpS.plot1d(chirpS.results[:, x], chirpS.t_b, title=title)
-
-mapbf = chirpS.plot2d(MIN=0, MAX=0.08, title=title)
-mapaf = chirpS.plot2d(chirpS.results, MIN=0, MAX=0.2, title=title)
-
+plots = chirpS.filter_example(filter_method=21, title=title, trim=100, x=x)
 
 # %% Long Chirp, 0.8-2.2MHz
 
@@ -86,17 +48,7 @@ chirpL = signal("signal_data/chirp_0822MHz_6u/longchirp.dat",
                 "signal_data/chirp_0822MHz_6u/bscan.dat")
 title = r'Chirp Pulse, $0.8\,$-$\,2.2\,$MHz, $6\,\mu$s'
 
-chirpL.match2d(remove_pulse=1, trim=-45)
-chirpL.wien(window=(100,10))
-
-plot = chirpL.plot1d(title=title)
-
-plotbf = chirpL.plot1d(chirpL.data_b[:, x], chirpL.t_b, title=title)
-plotaf = chirpL.plot1d(chirpL.results[:, x], chirpL.t_b, title=title)
-
-mapbf = chirpL.plot2d(MIN=0, MAX=0.08, title=title)
-mapaf = chirpL.plot2d(chirpL.results, MIN=0, MAX=0.2, title=title)
-
+plots = chirpL.filter_example(filter_method=21, title=title, trim=-45, x=x)
 
 # %% Barker 13, 1MHz
 
@@ -104,19 +56,8 @@ bark1 = signal("signal_data/barker_1MHz_13/signalbarker13.dat",
                "signal_data/barker_1MHz_13/bscanbarker13.dat")
 title = r'Barker Code (length $13$), $1\,$MHz'
 
-bark1.match2d(remove_pulse=1, trim=-300)
-# bark1.results[:700,:]=np.mean(abs(bark1.results[701:,:]))
-bark1.results[:700, :] = 0
-bark1.wien(window=(100, 10))
-
-plot = bark1.plot1d(title=title)
-
-plotbf = bark1.plot1d(bark1.data_b[:, x], bark1.t_b, title=title)
-plotaf = bark1.plot1d(bark1.results[:, x], bark1.t_b, title=title)
-
-mapbf = bark1.plot2d(MIN=0, MAX=0.2, title=title)
-mapaf = bark1.plot2d(bark1.results, MIN=0, MAX=0.4, title=title)
-
+plots = bark1.filter_example(filter_method=21, title=title, trim=-300, x=x,
+                             remove_matchedpulse=700)
 
 # %% Barker 13, 2MHz
 
@@ -124,71 +65,42 @@ bark2 = signal("signal_data/barker_2MHz_13/barker13.dat",
                "signal_data/barker_2MHz_13/bscanbarker13.dat")
 title = r'Barker Code (length $13$), $1\,$MHz'
 
-bark2.match2d(remove_pulse=1)
-bark2.wien(window=(100, 10))
-
-plot = bark2.plot1d(title=title)
-
-plotbf = bark2.plot1d(bark2.data_b[:, x], bark2.t_b, title=title)
-plotaf = bark2.plot1d(bark2.results[:, x], bark2.t_b, title=title)
-
-mapbf = bark2.plot2d(MIN=0, MAX=0.2, title=title)
-mapaf = bark2.plot2d(bark2.results, MIN=0.0, MAX=0.10, title=title)
-
+plots = bark2.filter_example(filter_method=21, title=title, x=x)
 
 # %% Golay 3, 2MHz
 
-# ~~~~~~~~~~~
+gtrim = 100
+gfilt = 21
+
 # MAIN SIGNAL
-# ~~~~~~~~~~~
 
 golay = signal("signal_data/golay/golay_a_l3_2.dat",
                "signal_data/golay/bscangolay2mhz_l3nc.dat")
 title = r'Golay Code (length $3$), $2\,$MHz'
 
-gtrim = 100
+plots = golay.filter_example(filter_method=gfilt, trim=gtrim,
+                             title=title, x=x,
+                             plotresults=(0, 0, 0, 0, 0))
 
-golay.match2d(remove_pulse=1, trim=gtrim)
-# golay.wien()
-
-plot = golay.plot1d(title=title)
-# plotbf = golay.plot1d(golay.data_b[:, x], golay.t_b,title=title)
-# plotaf = golay.plot1d(golay.results[:, x], golay.t_b,title=title)
-
-# mapbf = golay.plot2d(MIN=0, MAX=0.1, title=title)
-# mapaf = golay.plot2d(golay.results, MIN=0, MAX=0.2,title=title)
-
-# ~~~~~~~~~~~~~~~~~~~~
 # COMPLEMENTARY SIGNAL
-# ~~~~~~~~~~~~~~~~~~~~
 
 golayC = signal("signal_data/golay/golay_l3_2c.dat",
                 "signal_data/golay/bscangolay2mhz_l3c.dat")
+title = r'Golay Complementary Code (length $3$), $2\,$MHz'
 
-golayC.match2d(remove_pulse=1, trim=gtrim)
-# golayC.wien()
+plotsC = golayC.filter_example(filter_method=gfilt, trim=gtrim,
+                               title=title, x=x,
+                               plotresults=(0, 0, 0, 0, 0))
 
-# plot = golayC.plot1d(title=title)
-# plotbf = golayC.plot1d(golayC.data_b[:, x], golayC.t_b,title=title)
-# plotaf = golayC.plot1d(golayC.results[:, x], golayC.t_b,title=title)
-
-# mapbf = golayC.plot2d(MIN=0, MAX=0.1,title=title)
-# mapaf = golayC.plot2d(golayC.results, MIN=0, MAX=0.2,title=title)
-
-# ~~~~~~~~~~~~~~~~~~~~~~~
 # SUM OF FILTERED SIGNALS
-# ~~~~~~~~~~~~~~~~~~~~~~~
 
 golay_sum = signal("signal_data/golay/golay_a_l3_2.dat",
                    "signal_data/golay/bscangolay2mhz_l3nc.dat")
+title = r'Golay Code (length $3$), $2\,$MHz'
 
 golay_sum.results = golay.results + golayC.results
-# golay_sum.results[-200:, :] = 0
-golay_sum.wien(window=(100,10))
-# golay_sum.results[-200:, :] = np.nan # HMMM?
 
-plotbf = golay_sum.plot1d(golay.data_b[:, x], golay.t_b, title=title)
-plotaf = golay_sum.plot1d(golay_sum.results[:, x], golay_sum.t_b, title=title)
-
-mapbf = golay.plot2d(MIN=0, MAX=0.1, title=title)
-mapaf = golay_sum.plot2d(golay_sum.results, MIN=0.0, MAX=0.2, title=title)
+plotsS = golay_sum.filter_example(signal=golay_sum.results,
+                                  filter_method=0, remove_pulse=0,
+                                  title=title, x=x,
+                                  plotresults=(1, 1, 1, 1, 1))
